fdc08569e143fc87a57e31abe40081c6f2bcb71e
lucasHcc <lucashccgames@gmail.com>
2023-05-07 06:45:35 -0300

    first commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,61 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState , useEffect } from 'react';
+import { Text, View , Button, Alert} from 'react-native';
+import { styles } from './styles';
+import * as LocalAuthentication from 'expo-local-authentication'
 
 export default function App() {
+  const [ isAuthnticated, setIsAuthenticated ] = useState(false);
+
+  async function verifyAvaiableAuthetucation()
+  {
+    const compatible = await LocalAuthentication.hasHardwareAsync();
+    const types = await LocalAuthentication.supportedAuthenticationTypesAsync();
+    
+    const showTypes = types.map(type => LocalAuthentication.AuthenticationType[type]);
+    
+    console.log(showTypes);
+    
+  }
+
+   async function handleAuthentication()
+   {
+     const isBiometricEnrolled = await LocalAuthentication.isEnrolledAsync();
+     console.log(isBiometricEnrolled);
+
+     if(!isBiometricEnrolled)
+     {
+       return Alert.alert('Autenticacao', 'nenhuma biometrica encontrada')
+       
+     }     
+
+     const auth = await LocalAuthentication.authenticateAsync({
+      promptMessage: 'Login com biometria',
+      fallbackLabel: 'Biometria não reoconhecida',
+     });
+
+     setIsAuthenticated(auth.success)
+   }
+
+
+   useEffect(() => {
+     
+    verifyAvaiableAuthetucation();
+   },[])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>
+        usuario conectado:
+        {isAuthnticated? 'SIM' : 'Não'}
+      </Text>
+
+      <Button 
+       title='Entrar' 
+       onPress={handleAuthentication}
+       />
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
